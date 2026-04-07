7f5a8e02f56f7b7c78d88dfbbf57a19fb44ff044
Lucioafk <luciogabriel1550@gmail.com>
2026-04-07 14:03:58 -0300

    introduction to React in code

--- dio/src/App.js
+++ dio/src/App.js
@@ -1,10 +1,15 @@
-
+import button from "./components/button/index.js";
 
 function App() {
   return (
-    <div className="App">
-      Olá React
-    </div>
+      <div className="App"> 
+        <h1>Olá React</h1>
+        <button title="entrar">Entrar</button>
+        <button title="fechar">Fechar</button>
+        <button title="abrir">Abrir</button>
+        <button title="salvar">Salvar</button>
+      </div>
+     
   );
 }
 
